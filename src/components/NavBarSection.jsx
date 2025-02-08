356c2ef36f825bd36ba430ab6962908105426fce
lmabaso <lmabaso@student.wethinkcode.co.za>
2025-02-08 19:55:04 +0200

    Phase 2.1 clean up + adding favicon

--- src/components/NavBarSection.jsx
+++ src/components/NavBarSection.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const NavBarSection = () => {
   const navManu = [
-    { id: 1, name: "About Me"     },
-    { id: 2, name: "Education"    },
-    { id: 3, name: "Skills"       },
-    { id: 4, name: "Portfolio"    },
-    { id: 5, name: "Achievements" },
-    { id: 6, name: "Contact"      }
+    { id: 1, name: "About Me",      status: "active" },
+    { id: 2, name: "Education",     status: "active" },
+    { id: 3, name: "Skills",        status: "active" },
+    { id: 4, name: "Portfolio",     status: "active" },
+    { id: 5, name: "Achievements",  status: "active" },
+    { id: 6, name: "Contact",       status: "active" }
   ];
+
   return (
     <div className='font-[lato] bg-[#1a1a1a] min-w-[280px] w-[280px] relative flex flex-col rounded-xl'>
       <header className='flex justify-center items-center flex-col text-center p-5 border-b border-[#232526]'>
@@ -20,9 +21,10 @@ const NavBarSection = () => {
           Liberty <span className='text-[#aefd9c]'>Mabaso</span>
         </strong>
         <strong>Web Developer</strong>
-        </header>
-          <div className='w-full h-full flex flex-col '>
-            {navManu.map((option, idx) => (
+      </header>
+      <div className='w-full h-full flex flex-col '>
+        {navManu.map((option, idx) => (
+          option.status === "active" && (
             <button
               key={idx}
               className='cursor-pointer px-4 h-[50px] border-b border-[#232526] text-left'>
@@ -30,13 +32,14 @@ const NavBarSection = () => {
                 <span className='text-[.95rem] font-bold'>{option.name}</span>
               </div>
             </button>
-            ))}
-          </div>
-          <footer className='h-[50px] flex  justify-center gap-1.5 py-4 border-t border-[#232526]'>
-            <div>Light Mode</div>
-            <div>Curser effects</div>
-          </footer>
-        </div>
+          )
+        ))}
+      </div>
+      <footer className='h-[50px] flex  justify-center gap-1.5 py-4 border-t border-[#232526]'>
+        <div>Light Mode</div>
+        <div>Curser effects</div>
+      </footer>
+    </div>
   )
 }
 
