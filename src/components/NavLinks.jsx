15c0447599d570f76e90cb88a337cb60134b7d0c
lmabaso <lmabaso@student.wethinkcode.co.za>
2025-05-20 04:33:11 +0200

    Added personal interests, fixed the toggle sidebar variable

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -5,12 +5,12 @@ import IconContainer from './icons/IconContainer';
 import PropTypes from 'prop-types';
 
 const navLinks = [
-  { id: 1, name: "About",         label: "About Me",      status: "active", path: "/About"},
-  { id: 2, name: "Education",     label: "Education",     status: "active", path: "/Education"},
-  { id: 3, name: "Experience",    label: "Experience",    status: "active", path: "/Experience" },
-  { id: 4, name: "Skills",        label: "Skills",        status: "active", path: "/Skills"},
-  { id: 5, name: "Portfolio",     label: "Portfolio",     status: "active", path: "/Portfolio"},
-  { id: 6, name: "Achievements",  label: "Achievements",  status: "active", path: "/Achievements"}
+  { id: 1, name: "About",         label: "About Me",      status: "inactive", path: "/About"},
+  { id: 2, name: "Education",     label: "Education",     status: "inactive", path: "/Education"},
+  { id: 3, name: "Experience",    label: "Experience",    status: "inactive", path: "/Experience" },
+  { id: 4, name: "Skills",        label: "Skills",        status: "inactive", path: "/Skills"},
+  { id: 5, name: "Portfolio",     label: "Portfolio",     status: "inactive", path: "/Portfolio"},
+  { id: 6, name: "Achievements",  label: "Achievements",  status: "inactive", path: "/Achievements"}
   // { id: 7, name: "Contact",       label: "Contact",       status: "active", path: "/Contact" }
 ];
 
