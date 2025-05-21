835af843ed67509b26691556ea36961f0b025f71
lmabaso <lmabaso@student.wethinkcode.co.za>
2025-05-21 20:12:53 +0200

    Made the links have a scroll when the hight is small enough

--- src/components/navbar/NavBarSection.jsx
+++ src/components/navbar/NavBarSection.jsx
@@ -10,12 +10,12 @@ const NavBarSection = () => {
   return (
     <div
       id='NavBarSection'
-      className={`font-[lato] bg-[#1a1a1a] relative flex flex-col rounded-xl
+      className={`font-[lato] bg-[#1a1a1a] relative flex flex-col rounded-xl h-[calc(100vh-2rem)]
                   ${toggleSidebar ? ' min-w-[280px] w-[280px]' : ' min-w-[120px] w-[120px]'}
                 `}>
       <ToggleSidebar setToggleSidebar={setToggleSidebar} />
       <NavHeader toggleSidebar={toggleSidebar} />
-      <div className='overflow-y-auto [&::-webkit-scrollbar]:w-1
+      <div className='h-full overflow-y-auto [&::-webkit-scrollbar]:w-1
                     [&::-webkit-scrollbar-track]:bg-[#111111]
                     [&::-webkit-scrollbar-thumb]:bg-[#32343a]
                     dark:[&::-webkit-scrollbar-track]:bg-neutral-700
@@ -23,7 +23,16 @@ const NavBarSection = () => {
         <NavLinks toggleSidebar={toggleSidebar} />
       </div>
       <footer
-        className='sticky border-0 flex flex-row justify-center items-center py-2 gap-1.5 border-t border-[#232526] z-10'>
+        className='sticky flex flex-row justify-center items-center py-2 gap-1.5 border-t border-[#232526]'>
+        {/* <footer className='
+            fixed bottom-0
+            left-0
+            right-0
+            bg-[#1a1a1a]
+            border-t border-[#232526]
+            py-2 flex flex-row justify-center items-center gap-1.5
+            z-10'
+          > */}
         <ThemeButton />
       </footer>
     </div>
