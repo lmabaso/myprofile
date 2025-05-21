d85781ef5cb636b6b90a9cfb816c6deecbe276cd
lmabaso <lmabaso@student.wethinkcode.co.za>
2025-05-21 21:08:34 +0200

    Added theme toggle button

--- src/components/buttons/ThemeButton.jsx
+++ src/components/buttons/ThemeButton.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import IconContainer from "../icons/IconContainer";
 
 const ThemeButton = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'dark' : window.matchMedia('(prefers-color-scheme: dark)').matches
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light')
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode)
+  };
+
   return (
-    <>
-    <IconContainer name='Sun'   className="w-9 h-9 fill-[#6c757d] hover:fill-[#aefd9c]" />
-    <IconContainer name='Moon'  className="w-9 h-9 fill-[#6c757d] p-1 hover:fill-[#aefd9c]"/>
-    </>
+    <button  onClick={toggleTheme} >
+      {isDarkMode ?
+        <IconContainer name='Sun'   className="w-9 h-9 fill-[#6c757d] hover:fill-[#aefd9c]"/> :
+        <IconContainer name='Moon'  className="w-9 h-9 fill-[#6c757d] hover:fill-[#aefd9c] p-1"/>}
+    </button>
   )
 }
 
